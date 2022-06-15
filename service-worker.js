18d567b1b1517fd85055702e22ac765f5489a152
filipradenovic <fradenovic@gmail.com>
2022-06-15 00:00:28 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.308897d8b9d96a5cfa9d44502e21a83c.js"
+  "/precache-manifest.a96692c865357e558dfe312bdc265101.js"
 );
 
 self.addEventListener('message', (event) => {
